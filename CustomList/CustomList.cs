e700f07e7677f1f2967aee518aefefc43a26ea87
MikolaZhmendak <zhmendakmikola@yahoo.com>
2018-04-09 11:02:23 -0500

    added unit method test to see the size of my array

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -6,8 +6,52 @@ using System.Threading.Tasks;
 
 namespace CustomList
 {
-    class GenericList
+    public class MyCustomList<T>
     {
+        T[] tempValue;
+        int arraySize;
+        int count;
+
+
+        public MyCustomList()
+            {
+            arraySize = 10;
+            tempValue = new T[arraySize];
+
         //my generic list
+        // methods to create here:
+        // 1. Write a  method that will store values inside the array;
+        // 2. Wtite a method that will add objects in my list;
+        // 3. Write a method that will remove objects from my list;
+        // 4. Write a method that will add two instances of my custom lits;
+        // 5. Write a method that will zip two instances of my custom class. See exapmle in user story.
     }
+
+        //    Define the indexer to allow client code to use[] notation.
+        public T this[int i]
+        {
+            get
+            { 
+            return tempValue[i];
+            }
+            set
+            {
+                tempValue[i] = value;
+            }
+        }
+
+        public void StoreToList (T input)
+        {
+            
+         
+
+
+        }
+ 
+    }
+
 }
+
+
+
+    
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
 
 namespace UnitTestProject1
 {
@@ -7,8 +8,18 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Store_Int_PutIntInTheArray()
         {
+            //  int expectedValue;
+            MyCustomList<int> list = new MyCustomList<int>();
+            int value = 1;
+            list.StoreToList(value);
+
+            Assert.AreEqual(list[0], value);
+
+         }
+            
+            
         }
     }
-}
+
